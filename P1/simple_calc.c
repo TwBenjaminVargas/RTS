bca19b34c164b5c014a32a1a3b6e16faa40d32f0
TwBenjaminVargas <benjaminvargasoficial@gmail.com>
2026-03-03 11:45:20 -0300

    class 20260302

---
+++ P1/simple_calc.c
@@ -0,0 +1,67 @@
+/*
+ * Calculadora simple por línea de comandos.
+ *
+ * Uso:
+ *   ./calc <num1> <num2> <op>
+ *
+ * Donde:
+ *   <num1> y <num2> son números reales válidos.
+ *   <op> es un carácter:
+ *        'a' → suma
+ *        's' → resta
+ *        'm' → multiplicación
+ *        'd' → división
+ *
+ * El programa valida que los operandos sean numéricos.
+ * Si ocurre un error, termina con código distinto de 0.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double get_val(char*);
+
+int main(int argc, char *argv[])
+{
+    char* endptr;
+    double val1 = get_val(*(argv + 1));
+    double val2 = get_val(*(argv + 2));
+    double res = 0;
+    char op = **(argv + 3);
+
+    switch (op)
+    {
+    case 'a':
+        res = val1 + val2;     
+        break;
+    case 'm':
+        res= val1 * val2;
+        break;
+    case 'd':
+        res= val1/val2;
+        break;
+    case 's':
+        res= val1 - val2;
+        break;
+    default:
+        printf("Invalid operator\n");
+        return 1;
+    }
+
+    printf("%.2f\n",res);
+    return 0;
+}
+
+double get_val(char *str_ptr)
+{
+    char* endptr;
+    double val = strtod(str_ptr, &endptr);
+    
+    if(str_ptr == endptr || *endptr != '\0')
+    {
+        printf("Invalid operand: %s\n", str_ptr);
+        exit(1);
+    }
+    
+    return val;
+}
